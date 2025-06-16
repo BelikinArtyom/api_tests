0063f9c6a063dc38fe346b3073ba0a2620d3ef22
Artyom Belikin <biloplus@yandex.ru>
2025-06-16 22:44:35 +0300

    First push commit 2

--- src/test/java/ReformattedApiTests.java
+++ src/test/java/ReformattedApiTests.java
@@ -1,7 +1,11 @@
-import models.LoginBodyModel;
-import models.LoginResponseModel;
+import io.qameta.allure.restassured.AllureRestAssured;
+import models.lombok.LoginBodyLombokModel;
+import models.lombok.LoginResponseLombokModel;
+import models.pojo.LoginBodyModel;
+import models.pojo.LoginResponseModel;
 import org.junit.jupiter.api.Test;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
@@ -12,7 +16,7 @@ public class ReformattedApiTests {
 
 
     @Test
-    void successfulLoginTest() {
+    void successfulPojoLoginTest() {
 
         LoginBodyModel authData = new LoginBodyModel();
         authData.setEmail("eve.holt@reqres.in");
@@ -36,5 +40,95 @@ public class ReformattedApiTests {
 
             assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
 
+    }
+
+    @Test
+    void successfulLombokLoginTest() {
+
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+        authData.setApiKey("reqres-free-v1");
+
+        LoginResponseLombokModel response = given()
+                .body(authData)
+                .contentType(JSON)
+                .header("x-api-key", authData.getApiKey())
+                .log().uri()
+                .log().body()
+                .log().headers()
+
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+
+    @Test
+    void successfulAllureLombokLoginTest() {
+
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+        authData.setApiKey("reqres-free-v1");
+
+        LoginResponseLombokModel response = given()
+                .filter(new AllureRestAssured())
+                .log().uri()
+                .log().body()
+                .log().headers()
+                .body(authData)
+                .contentType(JSON)
+                .header("x-api-key", authData.getApiKey())
+
+
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+
+    @Test
+    void successfulCustomAllureLombokLoginTest() {
+
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+        authData.setApiKey("reqres-free-v1");
+
+        LoginResponseLombokModel response = given()
+                .filter(withCustomTemplates())
+                .log().uri()
+                .log().body()
+                .log().headers()
+                .body(authData)
+                .contentType(JSON)
+                .header("x-api-key", authData.getApiKey())
+
+
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
     }
 }
