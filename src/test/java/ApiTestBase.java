0df3a833c78b20fae202fe507de3432f0b212df9
Artyom Belikin <biloplus@yandex.ru>
2025-08-11 02:03:25 +0300

    234

--- src/test/java/ApiTestBase.java
+++ src/test/java/ApiTestBase.java
@@ -1,7 +1,9 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -28,5 +30,12 @@ public class ApiTestBase {
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 }
-
+    @AfterEach
+    void attach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.getVideoUrl();
+        Attach.addVideo();
+    }
 }
